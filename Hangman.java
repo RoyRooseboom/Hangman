c95bd8e2f05424397e9f9a18095edf5d63ce95f5
RoyRooseboom <brrooseboom@gmail.com>
2024-09-08 19:51:58 +0200

    We now check or we have guessed all the letters in the word

--- Hangman.java
+++ Hangman.java
@@ -14,7 +14,10 @@ public class Hangman
     static ArrayList<Character> lettersGuessed = new ArrayList<Character>();
     static ArrayList<Character> wrongLetters = new ArrayList<Character>();
     static ArrayList<Character> correctLetters = new ArrayList<Character>();
-    static String[] words = {"HELLO", "CAR", "BOOK", "RENTAL UNIT", "PARLEMENT"};
+    static ArrayList<Character> charactersFromCorrectWord = new ArrayList<Character>();
+    static String[] words = {"HELLO", "CAR", "BOOK", "RENTALUNIT", "PARLEMENT"};
+
+    static boolean continueGame = true;
 
     public static void main(String[] args)
     {
@@ -25,46 +28,55 @@ public class Hangman
         GetPlayerInput playerInput = new GetPlayerInput();
         ShowOutput output = new ShowOutput();
 
-        for(int i = 0; i < timesYouCanTry; i++)
+        while (continueGame) 
         {
-            CheckAwnser(correctWord, playerInput.GetInput(correctWord));
+            for(int i = 0; i < timesYouCanTry; i++)
+            {
+                CheckAwnser(correctWord, playerInput.GetInput(correctWord));
 
-            System.out.println("\033[H\033[2J");
-            // System.out.flush();
+                System.out.println("\033[H\033[2J");
+                // System.out.flush();
 
-            System.out.println("Guessed: " + lettersGuessed);
-            System.out.println("Wrong: " + wrongLetters);
-            System.out.println("Correct: " + correctLetters);
-            System.out.println("Word: " + correctWord);
+                System.out.println("Guessed: " + lettersGuessed);
+                System.out.println("Wrong: " + wrongLetters);
+                System.out.println("Correct: " + correctLetters);
+                System.out.println("Word: " + correctWord);
 
-        }        
+            }        
+        }
     }
 
-    private static void CheckAwnser(String word, String letter)
+    private static void CheckAwnser(String word, String letters)
     {
-        for(char c : letter.toCharArray())
+        charactersFromCorrectWord.removeAll(charactersFromCorrectWord);
+
+        for(char l : word.toCharArray())
+        {
+            charactersFromCorrectWord.add(l);
+        }
+
+        for(char c : letters.toCharArray())
         {
             if(!lettersGuessed.contains(c))
             {
                 lettersGuessed.add(c);
             }
 
-            if(word.contains(letter))
+            if(word.contains(letters))
             {
-                if(!correctLetters.contains(letter.charAt(0))) correctLetters.add(c);
+                if(!correctLetters.contains(letters.charAt(0))) correctLetters.add(c);
             } else
             {
-                if(!wrongLetters.contains(letter.charAt(0))) wrongLetters.add(c);
+                if(!wrongLetters.contains(letters.charAt(0))) wrongLetters.add(c);
             }
         }
 
-        if(letter.equals(word)) EndGame();
+        // This works, however the "flushing of the screen" removes this.
+        // we have to end the game here.
+        if(charactersFromCorrectWord.containsAll(correctLetters)) continueGame = false;
 
-        for(char lettersInWord : word.toCharArray())
-        {
-            if(!lettersGuessed.contains(lettersInWord)) continue;
-        }
-        
+        // If they typed to full word
+        if(letters.equals(word)) EndGame();        
     }
 
     private static void EndGame()
